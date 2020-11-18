1ce0b7d13c5ba5a113f6c956dd5240368e670b4f
Alexey <jafaralex@mail.ru>
2020-11-18 16:35:04 +0400

    Added enum homework

--- src/main/java/com/nc/autumn2020/solutions/enumHomework/EnumHomeworkSolution.java
+++ src/main/java/com/nc/autumn2020/solutions/enumHomework/EnumHomeworkSolution.java
@@ -15,13 +15,20 @@ public class EnumHomeworkSolution implements LessonApi {
 
         String[] days = x.split(" ");
         int maxIndex = -1;
+        /*
+        Week maxDay = Week.MONDAY; // Пока не придумал как всё сделать элегантно с compareTo
+         Поэтому пока вариант с ordinal
+        */
         for (String day: days
         ) {
             int ordinal = Week.valueOf(day.toUpperCase()).ordinal();
             if (maxIndex < ordinal){
                 maxIndex = ordinal;
             }
+           // if (maxDay.compareTo(Week.valueOf(day.toUpperCase())) > ){}
         }
         System.out.println(Week.valueOf(Week.values()[maxIndex].toString()).phrase());
+
+
     }
 }
